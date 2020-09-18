d0ed7b1e33c9f3e22ecca48173984a8ae322513e
John Mitropoulos <jmitropoulos@alumni.berklee.edu>
2020-09-18 05:31:34 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,8 +12,10 @@ def summon_captain_planet(planeteer_calls)
   new_calls
 end
 
-def long_planeteer_calls(calls)
-  calls.any?
+def long_planeteer_calls(planet_calls)
+  planet_calls.any? do |call|
+    call.length > 4
+  end
 end
 
 def find_the_cheese# code an argument here
