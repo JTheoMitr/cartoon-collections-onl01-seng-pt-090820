c2e968a2e8c6cecf5cf99551717046d7a4e4086e
John Mitropoulos <jmitropoulos@alumni.berklee.edu>
2020-09-18 05:33:14 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,7 +18,7 @@ def long_planeteer_calls(planet_calls)
   end
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(food_list)
+  food_list.include?
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
